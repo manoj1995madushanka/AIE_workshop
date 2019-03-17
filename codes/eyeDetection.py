bf91cdf91007c3fe5c35593fc252798489ffe1bc
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-03-17 16:05:32 +0530

    convert camera capture to grayscale

--- codes/eyeDetection.py
+++ codes/eyeDetection.py
@@ -4,4 +4,7 @@ camera = cv2.VideoCapture(0) #0 default camera
 
 while(True):
     ret,frame = camera.read()
-    cv2.imshow("LIVE")
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow("LIVE",frame)
+    cv2.imshow("LIVE",gray)
+    cv2.waitKey(1) #1 milisecond elay
