a8a64d422f4ff347c508006565897f8804883d4b
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-03-25 15:53:28 +0530

    face detection

--- codes/eyeDetection.py
+++ codes/eyeDetection.py
@@ -2,18 +2,18 @@ import cv2
 
 camera = cv2.VideoCapture(0) #0 default camera also we can use another video source like films
 
-face_clsfr = cv2.CascadeClassifier("../haarcascade_frontalface_default.xml")
+eye_clsfr = cv2.CascadeClassifier("../haarcascade_eye_tree_eyeglasses.xml")
 
 while(True):
     ret,frame = camera.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#detect color image as gray
 
     #below line similar to result = clsfr.predict(test_data)
-    faces = face_clsfr.detectMultiScale(gray)
+    faces = eye_clsfr.detectMultiScale(gray)
 
-    #detect facess
+    #detect eyes
     for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
 
     #cv2.imshow("LIVE",frame)
     cv2.imshow("LIVE",frame)
---
+++ codes/faceDetection.py
@@ -0,0 +1,21 @@
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+face_clsfr = cv2.CascadeClassifier("../haarcascade_frontalface_default.xml")
+
+while(True):
+    ret,frame = camera.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # detect color image as gray
+
+    # below line similar to result = clsfr.predict(test_data)
+    faces = face_clsfr.detectMultiScale(gray)
+
+    # detect facess
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # cv2.imshow("LIVE",frame)
+    cv2.imshow("LIVE", frame)
+    # cv2.imshow("LIVE",gray)
+    cv2.waitKey(1)  # 1 milisecond elay
